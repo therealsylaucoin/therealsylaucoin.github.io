f6f7fc05a13569baa54d29d1af3d300cc21ecc48
therealsylaucoin <sylaucoin@gmail.com>
2020-12-14 20:04:45 -0500

    monday project time work session

--- script.js
+++ script.js
@@ -1,11 +1,55 @@
 const app = {};
-app.switch = $('#modeSwitch');
+
+//function to make the details appear on hover for each individual project
+app.showDetails = () => { 
+    app.projects.forEach((project) => {
+        let img = project.img;
+        let details = project.details;
+
+        $(`.${img}`).mouseenter(
+            function(){
+                $(`.${details}`)
+                    .removeClass('animate__fadeOut')
+                    .addClass('animate__fadeIn')
+                    .addClass('visible');
+            }
+        )    
+
+        $(`.${img}`).mouseleave(
+            function(){
+                $(`.${details}`)
+                    .addClass('animate__fadeOut');
+                setTimeout(function(){
+                $(`.${details}`)
+                    .removeClass('visible')
+                    .removeClass('animate__fadeIn')}, 500);
+            }
+        )  
+    })
+}
+
+
+
+app.init = () => {    
+    AOS.init(); //initialize animations library
+    app.showDetails();
+};
+
+$(function(){
+    app.init();
+    
+});
 
 //array of all portfolio project classes
 app.projects = [
-    {
+    monster = {
         img: 'monsterImg',
-        details: 'monsterDetails'
+        details: 'monsterDetails',
+        name: 'Monster Mash',
+        description: `A classic memory card matching game, which includes two modes (friendly and scary), three difficulty levels and a move counter.`,
+        skills: ['jQuery', 'Sass', 'Html5'],
+        live: 'https://themonstermash.netlify.app/',
+        github: 'https://github.com/therealsylaucoin/monsterMash',
     },
     {
         img: 'obliqueImg',
@@ -24,35 +68,3 @@ app.projects = [
         details: 'shoeDetails'
     },
 ]
-
-//function to make the details appear on hover for each individual project
-app.showDetails = () => { 
-    app.projects.forEach((project) => {
-        let img = project.img;
-        let details = project.details;
-        console.log(img, details);
-
-        $(`.${img}`).hover(
-            function(){
-                //get the class name
-                $(`.${details}`).toggleClass('visible');
-
-            }
-        )    
-    })
-}
-
-
-
-app.init = () => {
-    //switch from light to dark
-    // app.switch.on('click', function(){
-    //     $('body').toggleClass('lightMode', 'darkMode');
-    // })
-    app.showDetails();
-};
-
-$(function(){
-    app.init();
-    
-});
