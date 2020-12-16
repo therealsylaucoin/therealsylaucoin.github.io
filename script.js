df3c6ababccd3580a55b25aa1210c1ba5d805cf3
therealsylaucoin <sylaucoin@gmail.com>
2020-12-15 22:31:21 -0500

    looking fresh :nail_care: lots of styling done.Text to add next

--- script.js
+++ script.js
@@ -1,52 +1,102 @@
 const app = {};
 
-//function to change the display image
+//Event handler to show the menu!
+//on click of button 
+    //toggle class menuShow menuHide
+$('.menuToggle').click(function(){
+    $('.menu')
+        .toggleClass('menuShow menuHide')
+})
+
+//function to add the scrolled class to the nav bar once it starts scrolling
+app.scroll = () => {
+    $(document).scroll(function () {
+        let nav = $(".navBarFixed");
+        nav.toggleClass('scrolled', $(this).scrollTop() > nav.height());
+    });
+};
+
+
+//change display images
+//hover in function
+app.hoverIn = (hoverElement) => {
+    $(`.${hoverElement}`)
+            .removeClass('animate__fadeOut')
+            .addClass('animate__fadeIn')
+}
+
+//hover out function
+app.hoverOut = (hoverElement) => {
+    $(`.${hoverElement}`)
+        .addClass('animate__fadeOut');
+    setTimeout(function(){
+    $(`.${hoverElement}`)
+        .removeClass('animate__fadeIn')}, 500);
+}
+
+//function to change the display image (mouse hover events)
 app.showHoverImg = () => { 
     app.projects.forEach((project) => {
-        const img = project.img;
-        const hover = project.hover;
-        $(`.${img}`).hover(
-            function(){
-                $(`.${hover}`)
-                    .removeClass('animate__fadeOut')
-                    .addClass('animate__fadeIn')
-                    .addClass('visible');
-            },
-            function(){
-                $(`.${hover}`)
-                    .addClass('animate__fadeOut');
-                setTimeout(function(){
-                $(`.${hover}`)
-                    .removeClass('visible')
-                    .removeClass('animate__fadeIn')}, 500);
-            }
-        )    
+        const imgEl = project.img;
+        const hoverEl = project.hover;
+        $(`.${imgEl}`)
+            .hover(
+                function(){
+                    app.hoverIn(hoverEl);
+                },
+                function(){
+                    app.hoverOut(hoverEl);
+                })
+            .focus(
+                function(){
+                    app.hoverIn(hoverEl);
+                })
+            .focusout(
+                function(){
+                    app.hoverOut(hoverEl);
+                })
     })
 }
 
-//function to show and hide the project details
+//Show details
+//function to show and hide the project details (mouse click event)
 app.showDetails = () => { 
-    $('.hover').click(function(){
+    $('.hover')
+    //show the details
+    .click(function(){
         app.name = $(this).data('name');
         $(`.${app.name}Details`)
+            .addClass('visible')
             .addClass('animate__fadeInUp')
             .removeClass('animate__fadeOutDown');
     })
+
+    //attempting to ^ replicate via keyboard!
+    .on('keydown', function(e){
+        if (e.key == 'Enter'){
+            console.log('keypress');
+        }
+        console.log('keypress');
+    })
+
+    //hide the details
     $('.fa-times').click(function(){
         app.name = $(this).data('name');
         $(`.${app.name}Details`)
             .addClass('animate__fadeOutDown')
+            .removeClass('animate__fadeInUp')
+        setTimeout(function(){
+            $(`.${app.name}Details`)
+                .removeClass('visible')
+        }, 800)
     })
 }
-
-//function to get medium articles
-
 
 app.init = () => {    
     AOS.init(); //initialize animations library
     app.showHoverImg();
     app.showDetails();
-    
+    app.scroll();
 };
 
 $(function(){
