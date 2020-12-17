e44bee4c482fe337ac487a0fb65923e827b0cefd
therealsylaucoin <sylaucoin@gmail.com>
2020-12-17 16:31:30 -0500

    fixed mobile portfolio - refactored js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ $('.menuToggle').click(function(){
         .toggleClass('menuShow menuHide')
 })
 
-//function to add the scrolled class to the nav bar once it starts scrolling
+////Function to add the scrolled class to the nav bar once it starts scrolling
 app.scroll = () => {
     $(document).scroll(function () {
         let nav = $(".navBarFixed");
@@ -21,78 +21,51 @@ $('#scrollUp').click(function(){
     $('html, body').animate({scrollTop:0}, '300');
 });
 
-//minimal mode switch (switch between original style and minimal style)
+//Event listener for Minimal mode switch (switch between original style and minimal style)
 $('.switch').click(function(){
     $('body').toggleClass('minimal');
     $('body').hasClass('minimal') ? $('.switchTxt').html('Switch to original style.') : $('.switchTxt').html('Switch to minimal style.') 
 })
 
 ////FUNCTIONALITY to show individual project details
-//function to show and hide the project details (mouse click event)
-app.showDetails = () => { 
-    $('.project')
-    //show the details
+//function to show and hide the project details (mouse click & keypress event)
+app.showDetails = (project) => {
+    app.name = project.data('name');
+    $(`.${app.name}Details`)
+        .toggleClass('animate__fadeOutDown animate__fadeInUp')
+        setTimeout(function(){
+            $(`.${app.name}Details`).toggleClass('visible');
+        }, 400)
+}
+
+//Event listeners calling the showDetails function
+$('.project')
     .click(function(){
-        app.name = $(this).data('name');
-        $(`.${app.name}Details`)
-            .show()
-            .addClass('animate__fadeInUp')
-            .removeClass('animate__fadeOutDown');
-    })
-    //replicate the above via keyboard!
-    $('.project').on('keypress', function(e){
-        if (e.key == 'Enter'){
-            app.name = $(this).data('name');
-            $(`.${app.name}Details`)
-                .show()
-                .addClass('animate__fadeInUp')
-                .removeClass('animate__fadeOutDown');
+        if (window.innerWidth > 525 ){
+            app.showDetails($(this));
         }
     })
-    app.hideDetails();
-}
-
-app.hideDetails = () => {
-    $('.closeDetails').click(function(){
-        $('.monsterDetails')
-            console.log('click')
-            .removeClass('animate__fadeInUp');
+    //replicate the above via keyboard!
+    .on('keypress', function(e){
+        if (e.key == 'Enter'){
+            app.showDetails($(this));
+        }
     })
+
+//function to remove fade classes on mobile and add class visible
+app.mobileClass = () => {
+    if (window.innerWidth < 525 ){
+        $('.details')
+            .addClass('visible')
+            .removeClass('animate__fadeOutDown');
+    }
 }
 
-// app.hideDetails = () => {
-//     //hide the details
-//     $('.closeDetails')
-//         .click(function(){
-//             app.name = $(this).data('name');
-//             $(`.${app.name}Details`)
-//             .hide()
-//             .addClass('animate__fadeOutDown')
-//             .removeClass('animate__fadeInUp')
-//         setTimeout(function(){
-//             $(`.${app.name}Details`)
-//                 .removeClass('visible')
-//         },  800)
-//         })
-//          //replicate the above via keyboard!
-//         .on('keypress', function(e){
-//             if (e.key == 'Enter'){
-//             app.name = $(this).data('name');
-//             $(`.${app.name}Details`)
-//                 .addClass('animate__fadeOutDown')
-//                 .removeClass('animate__fadeInUp')
-//             setTimeout(function(){
-//                 $(`.${app.name}Details`)
-//                     .removeClass('visible')
-//             },  800)
-//         }
-//     })
-// }
 
 app.init = () => {    
     AOS.init(); //initialize animations library
-    app.showDetails();
     app.scroll();
+    app.mobileClass();
 };
 
 $(function(){
