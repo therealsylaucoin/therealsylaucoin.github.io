1388a210d8a8a50f1a6bcbf5fbef20e4cdfb7de3
therealsylaucoin <sylaucoin@gmail.com>
2020-12-18 11:17:30 -0500

    content inserted!  :black_nib:

--- script.js
+++ script.js
@@ -1,12 +1,50 @@
 const app = {};
+app.textArray = [' Front-End Developer', ' Hobby Dabbler', ' People Person'];
+
+
+//Function for text typing effect (in about section)
+// Type Effect
+app.typeEffect = () => {
+    const text = document.querySelector('.type');
+    let arrayIndex = 0;
+    let charIndex = 0;
+    
+    function type() {
+        if (charIndex < app.textArray[arrayIndex].length) {
+            // start inserting the text one character at a time with a .2ms delay
+            text.textContent += app.textArray[arrayIndex].charAt(charIndex);
+            charIndex++;
+            setTimeout(type, 100);
+        } else {
+            setTimeout(backspace, 800)
+        }
+    }
+    
+    function backspace() {
+        if (charIndex > 0) {
+            text.textContent = app.textArray[arrayIndex].substring(0, charIndex-1);
+            charIndex--;
+            setTimeout(backspace, 100);
+        }
+        else {
+            arrayIndex++;
+            if (arrayIndex >= app.textArray.length) arrayIndex = 0;
+            setTimeout(type, 100 + 1100);
+        }
+    }
+    
+    setTimeout(type, 500);
+};
+
 
 //Event listener to show the menu!
 //on click of button 
-    //toggle class menuShow menuHide
+//toggle class menuShow menuHide
 $('.menuToggle').click(function(){
     $('.menu')
         .toggleClass('menuShow menuHide')
 })
+
 
 ////Function to add the scrolled class to the nav bar once it starts scrolling
 app.scroll = () => {
@@ -65,6 +103,7 @@ app.mobileClass = () => {
 app.init = () => {    
     AOS.init(); //initialize animations library
     app.scroll();
+    app.typeEffect();
     app.mobileClass();
 };
 
